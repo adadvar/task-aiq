7fa8e21874c0168f065d9f2a79d62f9b0ab2dde7
adadvar <alirezadadvar24@gmail.com>
2025-03-01 11:33:27 +0330

    add translateY for scrollAnimation

--- src/hooks/useScrollAnimation.tsx
+++ src/hooks/useScrollAnimation.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from "react";
 
 function useScrollAnimation(
   maxRotation: number = 30,
-  rotationSpeed: number = 20
+  rotationSpeed: number = 20,
+  maxYTranslation: number = -235,
+  translationSpeed: number = 3
 ): React.RefObject<HTMLDivElement> {
   const elementRef = useRef<HTMLDivElement>(null);
 
@@ -12,12 +14,21 @@ function useScrollAnimation(
     const handleScroll = () => {
       if (elementRef.current) {
         const scrollY = window.scrollY;
+
+        // محاسبه چرخش
         let rotation = scrollY / rotationSpeed;
         rotation = Math.min(rotation, maxRotation);
         rotation = Math.max(rotation, 0);
+
+        // محاسبه جابجایی در محور Y
+        let translationY = maxYTranslation + scrollY / translationSpeed;
+        translationY = Math.min(translationY, 0);
+        translationY = Math.max(translationY, maxYTranslation);
+        console.log(translationY);
+
         elementRef.current.style.transform = `rotateX(${
           maxRotation - rotation
-        }deg)`;
+        }deg) translateY(${translationY}px)`;
       }
     };
 
@@ -26,7 +37,7 @@ function useScrollAnimation(
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [maxRotation, rotationSpeed]);
+  }, [maxRotation, rotationSpeed, maxYTranslation, translationSpeed]);
 
   return elementRef as React.RefObject<HTMLDivElement>;
 }
